8229ff4a51f1c97c9fcfea1f9585dea72b30c02b
yazzy4 <yazzy4@gmail.com>
2019-03-20 17:12:04 -0400

    Action sheet for edit, delete, working also seques to edit view, and save

--- FellowBloggerV2/Controllers/Blogger Controllers/EditPostViewController.swift
+++ FellowBloggerV2/Controllers/Blogger Controllers/EditPostViewController.swift
@@ -16,11 +16,37 @@ class EditPostViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
+    }
+    
+    private func updateUI(){
+        editBlogDescriptionTextView.text = blog.blogDescription
+    }
 
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        navigationItem.rightBarButtonItem?.isEnabled = false
+        guard let description = editBlogDescriptionTextView.text,
+            !description.isEmpty else {
+                showAlert(title: "Missing Fields", message: "Must enter text")
+                return
+        }
+        DBService.firestoreDB
+        .collection(BlogsCollectionKeys.CollectionKey)
+        .document(blog.documentId)
+            .updateData([BlogsCollectionKeys.BlogDescritionKey : description ]) {  (error) in
+                if let error = error {
+                    self.showAlert(title: "Editing Error", message: error.localizedDescription)
+                }
+                self.navigationItem.rightBarButtonItem?.isEnabled = true
+                self.performSegue(withIdentifier: "Unwind From Edit Blog", sender: self)
+            }
+                
+        }
         
     }
     
 
-   
 
-}
